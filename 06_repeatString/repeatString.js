ef210e499f0d2216ceb148021bec81cba972d636
Cahil Cowan <kevinc20000720@gmail.com>
2025-11-25 21:56:43 -0500

    All test passed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = ""
 
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    for (let i = 1; i <= num; i++)
+    {
+        str = str + string;
+    }
+
+    return str;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
